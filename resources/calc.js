f4c4d0c22505f6a98c09328581eaa20b26ae4db7
OSADCHYI <trkua01@gmail.com>
2023-01-28 02:36:16 +0100

    added calculate

---
+++ resources/calc.js
@@ -0,0 +1,43 @@
+function calculate() {
+	const num1 = +document.querySelector('[name="num1"]').value;
+	const num2 = +document.querySelector('[name="num2"]').value;
+	const op = document.querySelector('[name="op"]').value;
+	
+	let result;
+	
+	if (op === 'plus') {
+	       result = num1 + num2; 
+	} else if (op === 'minus') {
+	       result = num1 - num2 ; 
+	} else if (op === 'multiple') {
+	           result = num1 * num2; 
+	} else  {
+	         result = num1 / num2; 
+	}
+	
+	document.querySelector('.calc-result').value = result;
+}
+
+document.querySelector('.but').addEventListener('click', function ()  { 
+calculate(); 
+	
+	
+});
+
+document.querySelector('[name="op"]').addEventListener('change', function () {
+	calculate(); 
+	
+	
+});
+
+document.querySelector('[name="num1"]').addEventListener('input', function ()  { 
+calculate(); 
+	
+	
+});
+
+document.querySelector('[name="num2"]').addEventListener('input', function () {
+	calculate(); 
+	
+	
+});
