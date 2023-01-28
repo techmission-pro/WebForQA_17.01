be8a0d9300de6182c79160ff6fa2c21a2039d203
YaroslavNosov <jarik250games@gmail.com>
2023-01-28 16:39:02 +0200

    add calculator

--- resources/calc.js
+++ resources/calc.js
@@ -1,11 +1,39 @@
-document.querySelector('.calc-calculate').addEventListener('click',
+function calculate() {
 
-    const num1 = document.querySelector('[name="num1"]').value;
-    const num2 = document.querySelector('[name="num2"]').value;
-
-    const sum = num1 + num2;
-
-    document.querySelector('.calc-result').innerText = sum;
+ const num1 = +document.querySelector('[name="num1"]').value;
+ const num2 = +document.querySelector('[name="num2"]').value;
+ const op = document.querySelector('[name="op2"]').value;
  
+ let result;
+ 
+ if (op === 'plus') {
+  result = num1 + num2;
+ } else if (op === 'minus') {
+        result = num1 - num2;
+    } else if ( op === 'multiply') {
+        result = num1 * num2;
+    } else if ( op === 'divide') {
+     result = num1 / num2;
+    } else {
+        console.error('Wrong op!');  
+  alert('Wrong operation is selected');
+ }
+ 
+ document.querySelector('.calc-result').value = result;
+} 
+
+document.querySelector('.calc-calculate').addEventListener('click', funcion() {
+ calculate ()
 });
 
+document.querySelector('[name="op"]').addEventListener('click', funcion() {
+ calculate ()
+});
+
+document.querySelector('[name="num1"]').addEventListener('click', funcion() {
+ calculate ()
+});
+
+document.querySelector('.[name="num2"]').addEventListener('click', funcion() {
+ calculate ()
+});
