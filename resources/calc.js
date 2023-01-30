43bf9671d66d19d4d6360a6010af54eedf51cc97
Zaika_Ihor <teploservispl@gmail.com>
2023-01-30 22:55:36 +0200

    added automatic calculation

--- resources/calc.js
+++ resources/calc.js
@@ -1,29 +1,40 @@
 //console.log('Calc loaded');
-document.querySelector('.calc-calculate').addEventListener('click', function () {
-
+function calculate () {
    const num1 = +document.querySelector('[name=num1]').value;
    const num2 = +document.querySelector('[name=num2]').value;
-   const op = document.querySelector('[name=op]').value;
+   const op = document.querySelector('[name="op"]').value;
+
    let result;
+
    if (op === 'plus') {
       result = num1 + num2;
-   }
-
-   else if (op === 'multiply') {
+   }   else if (op === 'multiply') {
       result = num1 * num2
-   }
-   else if (op === 'divide') {
+   }   else if (op === 'divide') {
       result = num1 / num2
-   }
-   else if (op === 'minus') {
+   }   else if (op === 'minus') {
       result = num1 - num2
-   }
-   else {
+   }   else {
       console.error('Wrong op!');
       alert('Wrong operanion is selected');
-
    }
 
-   //const sum = num1 + num2;//
    document.querySelector('.calc-result').value = result;
+}
+
+//const sum = num1 + num2;//
+document.querySelector('.calc-calculate').addEventListener('click',function() {
+   calculate();
+});
+
+document.querySelector('[name="op"]').addEventListener('change',function() {
+   calculate();
+});
+
+document.querySelector('[name=num1]').addEventListener('input',function() {
+   calculate();
+});
+
+document.querySelector('[name=num2]').addEventListener('input',function() {
+   calculate();
 });
